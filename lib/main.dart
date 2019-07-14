969a285845cc53c7528e419d088458c83e3335e1
alte12 <ridaahyad@gmail.com>
2019-07-14 19:10:48 +0100

    First Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
-  home: Center(
-    child: Text('I Am Rich'),
-  ),
-));
+//The main function is the starting option for all flutter apps.
 
-
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text("I Am Rich"),
+        backgroundColor: Colors.blueGrey[900],
+      ),
+    ),
+  ));
+}
