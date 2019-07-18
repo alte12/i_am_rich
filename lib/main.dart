b9ebdd23226410683be39000651aabdca5e58f41
alte12 <ridaahyad@gmail.com>
2019-07-18 16:24:01 +0200

    Commit

--- lib/main.dart
+++ lib/main.dart
@@ -5,10 +5,15 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
     home: Scaffold(
-      appBar: AppBar(
-        title: Text("I Am Rich"),
-        backgroundColor: Colors.blueGrey[900],
-      ),
-    ),
+        appBar: AppBar(
+          title: Text("I Am Rich"),
+          backgroundColor: Colors.blueGrey,
+        ),
+        body: Center(
+          child: Image(
+              image: NetworkImage(
+                  "https://www.thegreatapps.com/application/upload/Apps/2018/04/i-am-rich-5.jpg")),
+        )),
   ));
 }
+//Commited
